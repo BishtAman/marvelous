1d200ed4a895e80059aa16715b4707c6ba46d632
Aman Bisht <104513010+BishtAman@users.noreply.github.com>
2024-08-16 13:59:32 +0530

    changes video settings

--- src/components/base/base-elements/base-video.component.jsx
+++ src/components/base/base-elements/base-video.component.jsx
@@ -16,7 +16,7 @@ export const BaseVideo = () => {
 
       <iframe
         className="absolute top-0 h-[100vh] sm:hidden w-full z-[-99]"
-        src="https://www.youtube.com/embed/Lgj7_r6-FhU?autoplay=1&loop=1&playlist=Lgj7_r6-FhU&controls=0"
+        src="https://www.youtube.com/embed/Lgj7_r6-FhU?autoplay=1&loop=1&playlist=Lgj7_r6-FhU&controls=0&modestbranding=1&showinfo=0&iv_load_policy=3&rel=0&mute=1&vq=hd1080"
         title="YouTube Shorts video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
